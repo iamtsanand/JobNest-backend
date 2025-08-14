12d5dbc439138ba4ea838172054b46390d2e26b6
Tanpreet Singh <121126869+iamtsanand@users.noreply.github.com>
2025-08-14 16:31:08 +0530

    Update server.js for vercel deployment

--- server.js
+++ server.js
@@ -36,7 +36,9 @@ app.get('/', (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server is running on Port ${PORT}`);
-});
+// const PORT = process.env.PORT || 5000;
+// app.listen(PORT, () => {
+//     console.log(`Server is running on Port ${PORT}`);
+// });
+
+export default app;
